c06959ef22c755fb53132057168c8ee1599bda59
Israel Medina <isra2dave@gmail.com>
2020-12-16 19:53:17 -0300

    Added NewLocationForm component

---
+++ src/components/forms/new-location-form/NewLocationForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewLocationForm extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default NewLocationForm
