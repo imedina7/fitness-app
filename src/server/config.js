294cbb18b3af51e065dec5ad78395a5078dc3854
Israel Medina <isra2dave@gmail.com>
2020-12-17 12:13:23 -0300

    Added cors policy

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,8 @@
 const config = {
   PORT: process.env.PORT || 8080,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://localhost/fitness',
-  G_APIKEY: process.env.G_APIKEY || null
+  G_APIKEY: process.env.G_APIKEY || null,
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || ''
 }
 
 export default config;
--- src/server/web.js
+++ src/server/web.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import config from './config.js';
 
 import ApiRouterV1 from './api/v1/router.js';
@@ -12,6 +13,21 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
+
+let whitelist = ['http://localhost:5000', `https://${config.HEROKU_APP_NAME}.herokuapp.com`];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    if(!origin) return callback(null, true);
+    if(whitelist.indexOf(origin) === -1){
+      var message = 'The CORS policy for this origin doesn\'t ' +
+                'allow access from the particular origin.';
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(express.json());
 
 app.use('/', express.static( join(__dirname, '/../../build')));
