1c974e17ba6df8344163b47ebbbe654913990021
Israel Medina <isra2dave@gmail.com>
2020-12-18 02:09:03 -0300

    More linting v3....

--- src/components/layout/sidebar/__test__/Sidebar.test.js
+++ src/components/layout/sidebar/__test__/Sidebar.test.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Sidebar from '../Sidebar';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import { render, cleanup } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
+import Sidebar from '../Sidebar';
 import '@testing-library/jest-dom/extend-expect';
 
 afterEach(cleanup);
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<BrowserRouter><Sidebar /></BrowserRouter>, div);
-})
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <BrowserRouter>
+      <Sidebar />
+    </BrowserRouter>,
+    div,
+  );
+});
